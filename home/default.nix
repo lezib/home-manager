096b940e11ee0980c03b3888e8b3b33013180ddd
maurin Audenard <maurin@audenard.com>
2025-06-16 21:50:10 +0200

    remove duplicate of tmux configuration

--- home/default.nix
+++ home/default.nix
@@ -1,16 +1,6 @@
 { lib, pkgs, inputs, ... }:
 let 
   reader = path: lib.fileContents path ;
-  minimal-tmux-status = pkgs.tmuxPlugins.mkTmuxPlugin {
-      pluginName = "minimal";
-      version = "unstable-2023-01-06";
-      src = pkgs.fetchFromGitHub {
-        owner= "niksingh710";
-        repo= "minimal-tmux-status";
-        rev= "de2bb049a743e0f05c08531a0461f7f81da0fc72";
-        hash= "sha256-0gXtFVan+Urb79AjFOjHdjl3Q73m8M3wFSo3ZhjxcBA=";
-      };
-    };
 in
 {
   home = {
@@ -48,16 +38,6 @@ in
     fzf = {
       enable = true;
       enableZshIntegration = true;
-    };
-
-    tmux = {
-      enable = true;
-      plugins = [
-        { 
-	  plugin = minimal-tmux-status;
-	}
-      ];
-      extraConfig = reader ../config/tmux.conf;
     };
 
   };
